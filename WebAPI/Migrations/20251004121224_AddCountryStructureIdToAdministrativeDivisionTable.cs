82435c1627d9abb06f6adac957023db4afe341b9
Bashir M Ali <baryce@gmail.com>
2025-10-04 15:13:09 +0300

    Finished Creating CountryStructure Pages and Functionality

---
+++ WebAPI/Migrations/20251004121224_AddCountryStructureIdToAdministrativeDivisionTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCountryStructureIdToAdministrativeDivisionTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CountryStructureId",
+                table: "AdministrativeDivisions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CountryStructureId",
+                table: "AdministrativeDivisions");
+        }
+    }
+}
