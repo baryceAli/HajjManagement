64a918bb89fdc95c6e2d60e183cc769998e033ef
Bashir M Ali <baryce@gmail.com>
2025-10-04 04:21:50 +0300

    Moving to Missing Model

---
+++ CoreBusiness/CountryStructure.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreBusiness
+{
+    public class CountryStructure
+    {
+        public int CountryStructureId { get; set; }
+        public string Name { get; set; }
+        public int? ParentCountryStructureId { get; set; }
+        public int CountryId { get; set; }
+    }
+}
