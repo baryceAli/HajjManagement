38a421821e85f48edc4fcd79e21d69946a40fc2c
Bashir M Ali <baryce@gmail.com>
2025-10-06 00:06:08 +0300

    Stop since it requires bag info

---
+++ HajjManagement/HajjManagement.Shared/Services/Custom/CountryStructureCustomService.cs
@@ -0,0 +1,80 @@
+﻿using CoreBusiness;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HajjManagement.Shared.Services.Custom
+{
+    public class CountryStructureCustomService : ICountryStructureCustomService
+    {
+        private readonly IGenericAPIService<CountryStructure> countryStructureService;
+
+        public CountryStructureCustomService(IGenericAPIService<CountryStructure> countryStructureService)
+        {
+            this.countryStructureService = countryStructureService;
+        }
+        public async Task<IEnumerable<CountryStructure>> GetParentAsync(int countryStructureId, List<CountryStructure> countryStructureList)
+        {
+            try
+            {
+                var result = new List<CountryStructure>();
+
+                async Task GetParentRecursive(int childId)
+                {
+                    var parent = countryStructureList
+                        .FirstOrDefault(c => c.CountryStructureId ==
+                            countryStructureList
+                                .Where(x => x.CountryStructureId == childId)
+                                .Select(x => x.ParentCountryStructureId)
+                                .FirstOrDefault());
+
+                    if (parent != null)
+                    {
+                        result.Add(parent);
+                        await GetParentRecursive(parent.CountryStructureId); // go up to next parent
+                    }
+                }
+
+                await GetParentRecursive(countryStructureId);
+                return result;
+            }
+            catch (Exception)
+            {
+
+                return new List<CountryStructure>();
+            }
+        }
+
+        public async Task<IEnumerable<CountryStructure>> GetChildrenAsync(int countryStructureId, List<CountryStructure> countryStructureList)
+        {
+            try
+            {
+                var result = new List<CountryStructure>();
+
+                async Task GetChildrenRecursive(int parentId)
+                {
+                    var children = countryStructureList
+                        .Where(c => c.ParentCountryStructureId == parentId)
+                        .ToList();
+
+                    foreach (var child in children)
+                    {
+                        result.Add(child);
+                        await GetChildrenRecursive(child.CountryStructureId); // recurse deeper
+                    }
+                }
+
+                await GetChildrenRecursive(countryStructureId);
+                return result;
+            }
+            catch (Exception)
+            {
+
+                return new List<CountryStructure>();
+            }
+        }
+    }
+}
